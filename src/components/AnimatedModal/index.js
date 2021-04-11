d0b77470908950add893e7d2e281c09c316a4065
Bryan Azevedo <bryan42damasceno@gmail.com>
2021-04-10 21:06:19 -0300

    ADD componente de SEO

--- src/components/AnimatedModal/index.js
+++ src/components/AnimatedModal/index.js
@@ -1,4 +1,3 @@
-import React from "react";
 import {
   Button,
   Modal,
@@ -11,12 +10,13 @@ import {
 } from "@material-ui/core";
 import { ExpandMore } from "@material-ui/icons";
 import { useStyles } from "./styles";
+import React, { useState } from "react";
 
-function AnimatedModal({ open, close, title, text1, text2, text3, videoLink }) {
+function AnimatedModal({ close, title, text1, text2, text3, videoLink }) {
   const classes = useStyles();
-
+  const [open, setOpen] = useState("");
   return (
-    <div className={classes.container} >
+    <div className={classes.container}>
       <Modal
         open={open}
         onBackdropClick={() => setOpen(false)}
@@ -24,56 +24,64 @@ function AnimatedModal({ open, close, title, text1, text2, text3, videoLink }) {
       >
         <Grow in={open} timeout={2500} className={classes.growContent}>
           <Fade in={open} timeout={2500}>
-              <Card className={classes.card}>
-                <CardContent>
-                  <div className={classes.cardContent}>
-                    <div className={classes.cardTitle}>
-                      <Typography variant="h5" style={{ color: "#3467eb", fontFamily: 'Roboto' }}>
-                        { title }
-                      </Typography>
-                      <ExpandMore
-                        onClick={close}
-                        fontSize="large"
-                        style={{cursor: "pointer"}}
-                      />
-                    </div>
-
-                    <div className={classes.cardBody}>
-                      <Typography style={{ fontSize: '12px', fontFamily: 'Roboto' }} >
-                        { text1 }
-                        <br />
-                        { text2 }
-                        <br />
-                        { text3 }
-                      </Typography>
-                    </div>
-
-                    <div className={classes.cardVideo}>
-
-                    </div>
-
-                    <div className={classes.cardPayments}>
-                      <Typography style={{ fontSize: '18px', fontFamily: 'Roboto', color: "#3467eb", fontWeight: 500 }} >
-                        Formas de pagamento
-                      </Typography>
-                    </div>
-
-                  </div>
-                </CardContent>
-
-                <CardActions>
-                  <div className={classes.buttonContainer}>
-                    <Button
-                      variant="contained"
-                      className={classes.button}
-                      onClick={close}
+            <Card className={classes.card}>
+              <CardContent>
+                <div className={classes.cardContent}>
+                  <div className={classes.cardTitle}>
+                    <Typography
+                      variant="h5"
+                      style={{ color: "#3467eb", fontFamily: "Roboto" }}
                     >
-                      Garanta sua vaga
-                    </Button>
-                    
+                      {title}
+                    </Typography>
+                    <ExpandMore
+                      onClick={close}
+                      fontSize="large"
+                      style={{ cursor: "pointer" }}
+                    />
                   </div>
-                </CardActions>
-              </Card>
+
+                  <div className={classes.cardBody}>
+                    <Typography
+                      style={{ fontSize: "12px", fontFamily: "Roboto" }}
+                    >
+                      {text1}
+                      <br />
+                      {text2}
+                      <br />
+                      {text3}
+                    </Typography>
+                  </div>
+
+                  <div className={classes.cardVideo}></div>
+
+                  <div className={classes.cardPayments}>
+                    <Typography
+                      style={{
+                        fontSize: "18px",
+                        fontFamily: "Roboto",
+                        color: "#3467eb",
+                        fontWeight: 500,
+                      }}
+                    >
+                      Formas de pagamento
+                    </Typography>
+                  </div>
+                </div>
+              </CardContent>
+
+              <CardActions>
+                <div className={classes.buttonContainer}>
+                  <Button
+                    variant="contained"
+                    className={classes.button}
+                    onClick={close}
+                  >
+                    Garanta sua vaga
+                  </Button>
+                </div>
+              </CardActions>
+            </Card>
           </Fade>
         </Grow>
       </Modal>
