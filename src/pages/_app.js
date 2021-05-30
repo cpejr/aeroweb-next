088454be9e704b95d798ec08d5a3f21892ac5981
RaphaelLeivas <raphaelhenrique2013@outlook.com>
2021-05-30 16:55:08 -0300

    Teste do SSR em produção

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,28 +1,29 @@
-// import "../styles/globals.css";
-
-// function MyApp({ Component, pageProps }) {
-//   return <Component {...pageProps} />;
-// }
-
-// export default MyApp;
-
-// SSR do next desabilitado
-import '../styles/globals.css'
-
-function SafeHydrate({ children }) {
-  return (
-    <div suppressHydrationWarning>
-      {typeof window === 'undefined' ? null : children}
-    </div>
-  )
-}
+// SSR habilitado
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <SafeHydrate>
-      <Component {...pageProps} />
-    </SafeHydrate>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
+
+// SSR do next desabilitado
+// import '../styles/globals.css'
+
+// function SafeHydrate({ children }) {
+//   return (
+//     <div suppressHydrationWarning>
+//       {typeof window === 'undefined' ? null : children}
+//     </div>
+//   )
+// }
+
+// function MyApp({ Component, pageProps }) {
+//   return (
+//     <SafeHydrate>
+//       <Component {...pageProps} />
+//     </SafeHydrate>
+//   );
+// }
+
+// export default MyApp;
