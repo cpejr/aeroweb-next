be9c94d61d1d9e3175e701cf3245d82ac07ce7a9
RaphaelLeivas <raphaelhenrique2013@outlook.com>
2021-05-30 17:05:46 -0300

    Desabilitando SSR novamente, teste falhou em produção

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,29 +1,29 @@
-// SSR habilitado
-import "../styles/globals.css";
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
-
-// SSR do next desabilitado
-// import '../styles/globals.css'
-
-// function SafeHydrate({ children }) {
-//   return (
-//     <div suppressHydrationWarning>
-//       {typeof window === 'undefined' ? null : children}
-//     </div>
-//   )
-// }
+// SSR do next habilitado
+// import "../styles/globals.css";
 
 // function MyApp({ Component, pageProps }) {
-//   return (
-//     <SafeHydrate>
-//       <Component {...pageProps} />
-//     </SafeHydrate>
-//   );
+//   return <Component {...pageProps} />;
 // }
 
 // export default MyApp;
+
+// SSR do next desabilitado
+import '../styles/globals.css'
+
+function SafeHydrate({ children }) {
+  return (
+    <div suppressHydrationWarning>
+      {typeof window === 'undefined' ? null : children}
+    </div>
+  )
+}
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <SafeHydrate>
+      <Component {...pageProps} />
+    </SafeHydrate>
+  );
+}
+
+export default MyApp;
